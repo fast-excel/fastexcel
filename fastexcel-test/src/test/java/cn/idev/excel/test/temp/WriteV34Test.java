fce80667af9916292973db186104baee2a4ff6d4
ian zhang <ian.zhangzhe@gmail.com>
2025-04-20 18:25:04 +0800

    fix: fix complex header
    Fix this case as three valid field in class DemoData

--- fastexcel-test/src/test/java/cn/idev/excel/test/temp/WriteV34Test.java
+++ fastexcel-test/src/test/java/cn/idev/excel/test/temp/WriteV34Test.java
@@ -50,11 +50,8 @@ public class WriteV34Test {
                 contentWriteCellStyle);
         
         // 这里 需要指定写用哪个class去写，然后写到第一个sheet，名字为模板 然后文件流会自动关闭
-        EasyExcel.write(fileName, DemoData.class)
-                .head(head())
-                .registerWriteHandler(horizontalCellStyleStrategy)
-                .sheet("模板")
-                .doWrite(data(1));
+        EasyExcel.write(fileName, DemoData.class).head(head()).registerWriteHandler(horizontalCellStyleStrategy)
+                .sheet("模板").doWrite(data(1));
     }
     
     private List<List<String>> head() {
