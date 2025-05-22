257156d765b16f4f173842d0ef3ad1e61744a1c8
ian zhang <ian.zhangzhe@gmail.com>
2025-05-22 22:35:48 +0800

    test: Add web input stream test
    1. extract the core method
    2. remove useless comment

--- fastexcel-test/src/test/java/cn/idev/excel/test/temp/read/CommentTest.java
+++ fastexcel-test/src/test/java/cn/idev/excel/test/temp/read/CommentTest.java
@@ -7,6 +7,7 @@ import cn.idev.excel.metadata.CellExtra;
 import cn.idev.excel.read.listener.ReadListener;
 import cn.idev.excel.support.ExcelTypeEnum;
 import com.alibaba.fastjson2.JSON;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,9 +16,6 @@ import java.io.File;
 import java.util.Arrays;
 import java.util.List;
 
-/**
- * @author Zhe Zhang
- **/
 
 public class CommentTest {
     
@@ -25,48 +23,38 @@ public class CommentTest {
     
     private final List<String> commentList = Arrays.asList("测试", "comment");
     
-    @Test
-    public void xlsxCommentTest() throws Exception {
-        File file = new File("src/test/resources/comment/comment.xlsx");
+    
+    private void runCommentTest(String filePath, ExcelTypeEnum excelType) throws Exception {
+        File file = new File(filePath);
         FastExcel.read(file, new ReadListener() {
             @Override
             public void invoke(Object data, AnalysisContext context) {
+                // 当前测试不关心数据读取
             }
             
             @Override
             public void doAfterAllAnalysed(AnalysisContext context) {
+                // 当前测试不关心读取完成后的逻辑
             }
             
             @Override
             public void extra(CellExtra extra, AnalysisContext context) {
                 LOGGER.info("读取到了一条额外信息:{}", JSON.toJSONString(extra));
                 if (extra.getType().equals(CellExtraTypeEnum.COMMENT)) {
-                    commentList.contains(extra.getText());
+                    Assertions.assertTrue(commentList.contains(extra.getText()));
                 }
             }
-        }).excelType(ExcelTypeEnum.XLSX).extraRead(CellExtraTypeEnum.COMMENT).sheet().doRead();
+        }).excelType(excelType).extraRead(CellExtraTypeEnum.COMMENT).sheet().doRead();
+    }
+    
+    
+    @Test
+    public void xlsxCommentTest() throws Exception {
+        runCommentTest("src/test/resources/comment/comment.xlsx", ExcelTypeEnum.XLSX);
     }
     
     @Test
     public void xlsCommentTest() throws Exception {
-        File file = new File("src/test/resources/comment/comment.xls");
-        FastExcel.read(file, new ReadListener() {
-            @Override
-            public void invoke(Object data, AnalysisContext context) {
-            }
-            
-            @Override
-            public void doAfterAllAnalysed(AnalysisContext context) {
-            }
-            
-            @Override
-            public void extra(CellExtra extra, AnalysisContext context) {
-                LOGGER.info("读取到了一条额外信息:{}", JSON.toJSONString(extra));
-                if (extra.getType().equals(CellExtraTypeEnum.COMMENT)) {
-                    commentList.contains(extra.getText());
-                }
-            }
-        }).excelType(ExcelTypeEnum.XLS).extraRead(CellExtraTypeEnum.COMMENT).sheet().doRead();
+        runCommentTest("src/test/resources/comment/comment.xls", ExcelTypeEnum.XLS);
     }
-    
 }
