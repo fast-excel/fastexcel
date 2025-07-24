f2bdd1509c61ae4462d1d5fce8c7b1cd7b5b8ff8
DeleiGuo <deleiguo@163.com>
2025-07-25 00:37:07 +0800

    feat: add support to set 1904 date system based on global configuration

--- fastexcel/src/main/java/cn/idev/excel/util/WorkBookUtil.java
+++ fastexcel/src/main/java/cn/idev/excel/util/WorkBookUtil.java
@@ -5,9 +5,6 @@ import cn.idev.excel.metadata.data.DataFormatData;
 import cn.idev.excel.metadata.data.WriteCellData;
 import cn.idev.excel.write.metadata.holder.WriteWorkbookHolder;
 import cn.idev.excel.write.metadata.style.WriteCellStyle;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import org.apache.poi.hssf.record.crypto.Biff8EncryptionKey;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.poifs.filesystem.POIFSFileSystem;
@@ -18,13 +15,19 @@ import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.streaming.SXSSFWorkbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openxmlformats.schemas.spreadsheetml.x2006.main.CTWorkbook;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 
 /**
  *
  */
 public class WorkBookUtil {
 
-    private WorkBookUtil() {}
+    private WorkBookUtil() {
+    }
 
     public static void createWorkBook(WriteWorkbookHolder writeWorkbookHolder) throws IOException {
         switch (writeWorkbookHolder.getExcelType()) {
@@ -39,11 +42,16 @@ public class WorkBookUtil {
                     }
                     return;
                 }
-                Workbook workbook;
-                if (writeWorkbookHolder.getInMemory()) {
-                    workbook = new XSSFWorkbook();
-                } else {
-                    workbook = new SXSSFWorkbook();
+                Workbook workbook = writeWorkbookHolder.getInMemory() ? new XSSFWorkbook() : new SXSSFWorkbook();
+                Boolean use1904windowing = writeWorkbookHolder.getGlobalConfiguration().getUse1904windowing();
+                if (use1904windowing != null) {
+                    CTWorkbook ctWorkbook;
+                    if (workbook instanceof XSSFWorkbook) {
+                        ctWorkbook = ((XSSFWorkbook) workbook).getCTWorkbook();
+                    } else {
+                        ctWorkbook = ((SXSSFWorkbook) workbook).getXSSFWorkbook().getCTWorkbook();
+                    }
+                    ctWorkbook.getWorkbookPr().setDate1904(use1904windowing);
                 }
                 writeWorkbookHolder.setCachedWorkbook(workbook);
                 writeWorkbookHolder.setWorkbook(workbook);
