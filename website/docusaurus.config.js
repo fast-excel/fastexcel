6b9d950b5375f82c86e5aeb4a908026ca8fcc1b7
psxjoy <psxjoy@apache.org>
2025-08-11 17:21:21 +0800

    Add Docusaurus site

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -20,18 +20,20 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://psxjoy.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/fastexcel/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'psxjoy', // Usually your GitHub org/user name.
+  projectName: 'fastexcel', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
+
+  deploymentBranch: 'gh-pages',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
