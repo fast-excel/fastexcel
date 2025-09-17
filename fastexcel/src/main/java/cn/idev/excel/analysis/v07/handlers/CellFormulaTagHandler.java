43056698d95b8c86794fe99af963ff15720bf94a
DeleiGuo <deleiguo@163.com>
2025-09-17 23:57:17 +0800

    feat: support shared formula in xlsx

--- fastexcel/src/main/java/cn/idev/excel/analysis/v07/handlers/CellFormulaTagHandler.java
+++ fastexcel/src/main/java/cn/idev/excel/analysis/v07/handlers/CellFormulaTagHandler.java
@@ -1,13 +1,14 @@
 package cn.idev.excel.analysis.v07.handlers;
 
+import cn.idev.excel.constant.ExcelXmlConstants;
 import cn.idev.excel.context.xlsx.XlsxReadContext;
 import cn.idev.excel.metadata.data.FormulaData;
 import cn.idev.excel.read.metadata.holder.xlsx.XlsxReadSheetHolder;
+import cn.idev.excel.util.StringUtils;
 import org.xml.sax.Attributes;
 
 /**
- * Cell Handler
- *
+ * Cell formula tag handler
  */
 public class CellFormulaTagHandler extends AbstractXlsxTagHandler {
 
@@ -15,13 +16,43 @@ public class CellFormulaTagHandler extends AbstractXlsxTagHandler {
     public void startElement(XlsxReadContext xlsxReadContext, String name, Attributes attributes) {
         XlsxReadSheetHolder xlsxReadSheetHolder = xlsxReadContext.xlsxReadSheetHolder();
         xlsxReadSheetHolder.setTempFormula(new StringBuilder());
+        xlsxReadSheetHolder.setTempSharedIndex(null);
+
+        // shared formula
+        String t = attributes.getValue(ExcelXmlConstants.ATTRIBUTE_T);
+        if (StringUtils.isBlank(t) || !ExcelXmlConstants.ATTRIBUTE_SHARED.equals(t)) {
+            return;
+        }
+        String si = attributes.getValue(ExcelXmlConstants.ATTRIBUTE_SHARED_INDEX);
+        if (StringUtils.isBlank(si)) {
+            return;
+        }
+        xlsxReadSheetHolder.setTempSharedIndex(si);
+
+        String ref = attributes.getValue(ExcelXmlConstants.ATTRIBUTE_REF);
+        if (StringUtils.isBlank(ref)) {
+            return;
+        }
+        xlsxReadSheetHolder
+                .getSharedFormula()
+                .put(si, xlsxReadSheetHolder.getTempFormula().toString());
     }
 
     @Override
     public void endElement(XlsxReadContext xlsxReadContext, String name) {
         XlsxReadSheetHolder xlsxReadSheetHolder = xlsxReadContext.xlsxReadSheetHolder();
+
+        String formulaValue = xlsxReadSheetHolder.getTempFormula().toString();
+        String sharedIndex = xlsxReadSheetHolder.getTempSharedIndex();
+        if (StringUtils.isNotBlank(sharedIndex)) {
+            if (StringUtils.isBlank(formulaValue)) {
+                formulaValue = xlsxReadSheetHolder.getSharedFormula().get(sharedIndex);
+            }
+            xlsxReadSheetHolder.getSharedFormula().put(sharedIndex, formulaValue);
+        }
+
         FormulaData formulaData = new FormulaData();
-        formulaData.setFormulaValue(xlsxReadSheetHolder.getTempFormula().toString());
+        formulaData.setFormulaValue(formulaValue);
         xlsxReadSheetHolder.getTempCellData().setFormulaData(formulaData);
     }
 
--- fastexcel/src/main/java/cn/idev/excel/read/metadata/holder/xlsx/XlsxReadSheetHolder.java
+++ fastexcel/src/main/java/cn/idev/excel/read/metadata/holder/xlsx/XlsxReadSheetHolder.java
@@ -5,6 +5,8 @@ import cn.idev.excel.read.metadata.holder.ReadSheetHolder;
 import cn.idev.excel.read.metadata.holder.ReadWorkbookHolder;
 import java.util.Deque;
 import java.util.LinkedList;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -36,6 +38,15 @@ public class XlsxReadSheetHolder extends ReadSheetHolder {
      */
     private StringBuilder tempFormula;
     /**
+     * Shared index for current label.
+     */
+    private String tempSharedIndex;
+    /**
+     * Shared formula for current sheet.
+     */
+    private Map<String, String> sharedFormula;
+
+    /**
      * excel Relationship
      */
     private PackageRelationshipCollection packageRelationshipCollection;
@@ -43,6 +54,7 @@ public class XlsxReadSheetHolder extends ReadSheetHolder {
     public XlsxReadSheetHolder(ReadSheet readSheet, ReadWorkbookHolder readWorkbookHolder) {
         super(readSheet, readWorkbookHolder);
         this.tagDeque = new LinkedList<String>();
+        this.sharedFormula = new ConcurrentHashMap<>();
         packageRelationshipCollection = ((XlsxReadWorkbookHolder) readWorkbookHolder)
                 .getPackageRelationshipCollectionMap()
                 .get(readSheet.getSheetNo());
